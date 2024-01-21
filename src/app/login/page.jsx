5f3ab186095c821f5b20322527d7432e28853e44
Rubayet-billah <rubayetbillah909@gmail.com>
2024-01-21 23:46:32 +0600

    login component created

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,9 +1,75 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 const LoginPage = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData((previousData) => ({
+      ...previousData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form data:", formData);
+  };
+
   return (
     <div>
-      <h1 className="text-4xl">This is login page</h1>
+      <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
+        {/* Email input */}
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Email address
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="john.doe@company.com"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        {/* Password inputs */}
+        <div className="mb-6">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="•••••••••"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        {/* Submit button */}
+        <button
+          type="submit"
+          className="text-white bg-violet-600 hover:bg-violet-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-sm w-full  px-5 py-2.5 text-center "
+        >
+          Login
+        </button>
+      </form>
     </div>
   );
 };
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -124,40 +124,12 @@ const RegisterPage = () => {
           />
         </div>
 
-        {/* Terms and conditions checkbox */}
-        {/* <div className="flex items-start mb-6">
-          <div className="flex items-center h-5">
-            <input
-              id="agreeTerms"
-              name="agreeTerms"
-              type="checkbox"
-              value={formData.agreeTerms}
-              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <label
-            htmlFor="agreeTerms"
-            className="text-sm font-medium text-gray-900 ms-2 dark:text-gray-300"
-          >
-            I agree with the{" "}
-            <a
-              href="#"
-              className="text-blue-600 hover:underline dark:text-blue-500"
-            >
-              terms and conditions
-            </a>
-            .
-          </label>
-        </div> */}
-
         {/* Submit button */}
         <button
           type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white bg-violet-600 hover:bg-violet-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-sm w-full px-5 py-2.5 text-center "
         >
-          Submit
+          Register
         </button>
       </form>
     </div>
