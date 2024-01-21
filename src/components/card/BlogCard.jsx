9ac5eb2a796e0ca43979a299b8ee9022d71b56b5
Rubayet-billah <rubayetbillah909@gmail.com>
2024-01-21 19:55:14 +0600

    tailwind class sorted

--- src/components/card/BlogCard.jsx
+++ src/components/card/BlogCard.jsx
@@ -5,11 +5,11 @@ import carImage from "@/assets/car.jpg";
 const BlogCard = () => {
   const imageUrl = "https://placekitten.com/800/400";
   return (
-    <div className="max-w-sm mx-auto bg-white rounded-md overflow-hidden shadow-md">
+    <div className="max-w-sm mx-auto overflow-hidden bg-white rounded-md shadow-md">
       <Image className="object-cover w-full" src={carImage} alt="Blog Image" />
       <div className="p-4">
-        <section className="flex justify-between items-start">
-          <div className="flex gap-2 items-center">
+        <section className="flex items-start justify-between">
+          <div className="flex items-center gap-2">
             <Image
               className="w-10 h-10 rounded-full"
               src={carImage}
@@ -20,25 +20,25 @@ const BlogCard = () => {
               <p className="text-sm text-gray-500">25 April 2023</p>
             </div>
           </div>
-          <div className="bg-green-400 text-white px-2 py-1 rounded-full text-xs w-fit font-bold">
+          <div className="px-2 py-1 text-xs font-bold text-white bg-green-400 rounded-full w-fit">
             Technology
           </div>
         </section>
         {/* Blog Title */}
-        <h2 className="text-2xl font-bold my-2">Blog Post Title</h2>
+        <h2 className="my-2 text-2xl font-bold">Blog Post Title</h2>
 
         {/* Short Description */}
-        <p className="text-gray-700 mb-4">
+        <p className="mb-4 text-gray-700">
           Short description or summary of the blog post goes here. You can
           provide a brief overview of the content.
         </p>
 
         {/* Action Buttons */}
         <div className="flex justify-end">
-          <button className="bg-blue-500 text-white px-4 py-2 mr-2 rounded-md">
+          <button className="px-4 py-2 mr-2 text-white bg-blue-500 rounded-md">
             Read More
           </button>
-          <button className="bg-gray-500 text-white px-4 py-2 rounded-md">
+          <button className="px-4 py-2 text-white bg-gray-500 rounded-md">
             Share
           </button>
         </div>
