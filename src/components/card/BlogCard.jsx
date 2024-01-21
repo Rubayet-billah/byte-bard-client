5db9f5d77d646cea41f455fb1dc2e32407d6cfa2
Rubayet-billah <rubayetbillah909@gmail.com>
2024-01-21 20:06:49 +0600

    blog card designed

--- src/components/card/BlogCard.jsx
+++ src/components/card/BlogCard.jsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import React from "react";
 import carImage from "@/assets/car.jpg";
+import { GrNext } from "react-icons/gr";
 
 const BlogCard = () => {
-  const imageUrl = "https://placekitten.com/800/400";
   return (
     <div className="max-w-sm mx-auto overflow-hidden bg-white rounded-md shadow-md">
       <Image className="object-cover w-full" src={carImage} alt="Blog Image" />
       <div className="p-4">
+        {/* Blog Author */}
         <section className="flex items-start justify-between">
           <div className="flex items-center gap-2">
             <Image
@@ -17,7 +18,7 @@ const BlogCard = () => {
             />
             <div>
               <h3 className="font-bold">Eren Yeager</h3>
-              <p className="text-sm text-gray-500">25 April 2023</p>
+              <p className="text-xs text-gray-500">25 April 2023</p>
             </div>
           </div>
           <div className="px-2 py-1 text-xs font-bold text-white bg-green-400 rounded-full w-fit">
@@ -35,11 +36,8 @@ const BlogCard = () => {
 
         {/* Action Buttons */}
         <div className="flex justify-end">
-          <button className="px-4 py-2 mr-2 text-white bg-blue-500 rounded-md">
-            Read More
-          </button>
-          <button className="px-4 py-2 text-white bg-gray-500 rounded-md">
-            Share
+          <button className="flex items-center gap-1 font-bold duration-150 hover:scale-105">
+            Read Full <GrNext className="font-extrabold" />
           </button>
         </div>
       </div>
