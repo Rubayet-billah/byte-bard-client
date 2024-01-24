7d5e5e40e74b5970c03a7819be34d544c42fa699
Rubayet-billah <rubayetbillah909@gmail.com>
2024-01-24 22:50:07 +0600

    image resize

--- src/components/blog/Blogs.jsx
+++ src/components/blog/Blogs.jsx
@@ -21,7 +21,7 @@ const Blogs = () => {
 
   return (
     <div>
-      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-8">
+      <section className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {posts.map((post) => (
           <BlogCard key={post._id} post={post} />
         ))}
